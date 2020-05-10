57822dc996d3e2e7c987297798a20326f2217f47
515hikaru <12kojima.takahiro@gmail.com>
2020-05-10 22:40:01 +0900

    Revert "Merge pull request #8 from 515hikaru/revert-7-feature/kagikakko"
    This reverts commit d72d542b868fd57dd5286ae24ea9563e1e722911, reversing
    changes made to 4d393cf0ee2889dc3c051fcf1b4cd0fd74e5c4ad.

--- src/eval.js
+++ src/eval.js
@@ -16,7 +16,16 @@ export function parseDocumentNode (node) {
       return `[chapter:${text}]\n`
     }
     case 'paragraph': {
-      const results = ['　']
+      const results = []
+      contents.flat().forEach(element => {
+        const text = parseDocumentNode(element)
+        results.push(text)
+      })
+      const result = results.join('')
+      return '　' + result + '\n'
+    }
+    case 'conversation': {
+      const results = []
       contents.flat().forEach(element => {
         const text = parseDocumentNode(element)
         results.push(text)
@@ -40,6 +49,11 @@ function main () {
 ようよう白くなりry
 
 枕草子をいきなり空で書くなんて無理だったわ。
+
+「会話文」
+
+「改行
+もできる？」
 `
   const result = parser.parse(doc)
   console.log(parseDocumentNode(result))
