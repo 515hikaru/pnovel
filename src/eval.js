8d73e803969fe886ed1d219285025a37e2d4c755
515hikaru <12kojima.takahiro@gmail.com>
2020-05-10 17:01:31 +0900

    add parse script

--- src/eval.js
+++ src/eval.js
@@ -1,10 +1,47 @@
 const parser = require('../dist/parser')
 
-const doc =`foo
+const doc = `# foo
+
+foo
 bar
 
 boo
 `
 
-result = parser.parse(doc);
-console.log(result);
+function parseDocumentNode (node) {
+  const { type, contents } = node
+  switch (type) {
+    case 'doc': {
+      const results = []
+      contents.forEach(element => {
+        const text = parseDocumentNode(element)
+        results.push(text)
+      })
+      return results.join('')
+    }
+    case 'header': {
+      const text = parseDocumentNode(contents[0])
+      return `[chapter:${text}]\n`
+    }
+    case 'paragraph': {
+      const paragraphs = []
+      contents.flat().forEach(element => {
+        const text = parseDocumentNode(element)
+        paragraphs.push(text)
+      })
+      const paragraph = paragraphs.join('')
+      return paragraph + '\n'
+    }
+    case 'chars':
+      return contents
+    case 'break':
+      return '\n'
+    default:
+      return ''
+  }
+}
+
+const result = parser.parse(doc)
+
+console.log(parseDocumentNode(result))
+console.log(JSON.stringify(result))
