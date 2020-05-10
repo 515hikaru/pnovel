bb9cf72a56dc473c6b2229b4fc34dc9605ef3c5a
515hikaru <12kojima.takahiro@gmail.com>
2020-05-10 23:07:29 +0900

    修正
    - 例の文章の修正
    - エントリーポイントの分離 refs #6

---
+++ src/main.js
@@ -0,0 +1,27 @@
+import { parse } from '../parser/parser'
+import { parseDocumentNode } from './eval'
+
+function main () {
+  const doc = `# はじめに
+
+春はあけぼの
+ようよう白くなりry
+
+枕草子をいきなり空で書くなんて無理だったわ。
+
+「会話文」
+
+「改行
+もできる？」
+
+文章でも改行をしたいときは
+
+
+と2行あけます。
+`
+  const result = parse(doc)
+  console.log(parseDocumentNode(result))
+  console.log(JSON.stringify(result))
+}
+
+main()
