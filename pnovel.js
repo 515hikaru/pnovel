8e96552ae68f772441bd0bf6331d8355a0704f23
Takahiro Kojima <12kojima.takahiro@gmail.com>
2020-05-10 22:37:12 +0900

    Revert "会話文の段落を検知"

--- pnovel.js
+++ pnovel.js
@@ -3,30 +3,26 @@
  */
 start = doc
 
-doc = block: block + {
-  return { type: "doc", contents: block };
+doc = block:block+ {
+  return { type: "doc", contents: block};
 }
 
 block = header / paragraph / blankline
 
-header = prefix: "#" + " " textline: textline {
-  return { type: "header", contents: textline }
+header = prefix:"#" + " " textline:textline {
+    return { type: "header", contents: textline }
 }
 
-paragraph = textline: textline + blankline ? {
-  return { type: "paragraph", contents: textline }
+paragraph = textline:textline+blankline? {
+    return { type: "paragraph", contents: textline}
 }
 
-conversation = prefix: "「" textline: textline + blankline ? {
-  return { type: "conversation", contents: textline }
-}
-
-textline = inline: inline + blankline ? {
+textline = inline:inline+ blankline? {
   return inline;
 }
 
-inline = char: char + {
-  return { type: "chars", contents: char.join("") };
+inline = char:char+ {
+  return { type: "chars" , contents: char.join("") };
 }
 
 char = [^\n]
