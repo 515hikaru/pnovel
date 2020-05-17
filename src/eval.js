6e16a209c2ae963e99bebcb9955f71074c1ff1a6
515hikaru <12kojima.takahiro@gmail.com>
2020-05-17 17:57:53 +0900

    minimum なパーサー完成

--- src/eval.js
+++ src/eval.js
@@ -10,29 +10,17 @@ export function parseDocumentNode (node) {
       return results.join('')
     }
     case 'header': {
-      const text = parseDocumentNode(contents[0])
+      const text = contents
       return `[chapter:${text}]\n`
     }
-    case 'paragraph': {
-      const results = []
-      contents.flat().forEach(element => {
-        const text = parseDocumentNode(element)
-        results.push(text)
-      })
-      const result = results.join('')
-      return '　' + result + '\n'
+    case 'sentence': {
+      const text = contents
+      return '　' + text + '\n'
     }
-    case 'conversation': {
-      const results = []
-      contents.flat().forEach(element => {
-        const text = parseDocumentNode(element)
-        results.push(text)
-      })
-      const result = results.join('')
-      return '「' + result + '\n'
+    case 'speaking': {
+      const text = contents
+      return text + '\n'
     }
-    case 'chars':
-      return contents
     case 'break':
       return '\n'
     default:
