12fcbd3a570c58f5f8f58d18860323255e497531
515hikaru <12kojima.takahiro@gmail.com>
2020-05-23 12:34:25 +0900

    setting typescript

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,22 @@
 module.exports = {
-  entry: './src/main.js',
+  entry: './src/main.ts',
   target: 'node',
   mode: 'production',
   module: {
     rules: [
       {
         test: /\.js$/,
-        exclude: /node_modules/
+        exclude: /node_modules/,
+        use: {
+          loader: "ts-loader",
+          options: {
+            transpileOnly: true,
+          }
+        }
       }
     ]
   },
   resolve: {
-    extensions: [ '.js' ]
+    extensions: [ '.js', '.ts' ]
   }
 };
