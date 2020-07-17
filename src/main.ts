54978aa3899374b0cba9ab8965906847d02454e5
515hikaru <12kojima.takahiro@gmail.com>
2020-07-18 00:59:02 +0900

    Bump version

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ import { Command } from 'commander'
 import { parse } from '../parser/parser'
 import { parseEntireDocument } from './eval'
 
-const VERSION = 'v0.6.1-dev'
+const VERSION = 'v0.6.1'
 
 // https://stackoverflow.com/questions/42056246/node-js-process-stdin-issues-with-typescript-tty-readstream-vs-readablestream
 const stdin: any = process.stdin
