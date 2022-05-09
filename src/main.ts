8e26ef705b456bebc7585f2405448bcdf5d2be4f
Takahiro KOJIMA <12kojima.takahiro@gmail.com>
2022-05-09 21:37:25 +0900

    Bump version

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ import { parse } from "../parser/parser"
 import { PixivNovelTransformer } from "./pixivNovelTransformer"
 import { NarouSyosetsuTransformer } from "./narouSyosetsuTransformer"
 
-const VERSION = "v0.6.8-dev"
+const VERSION = "v0.6.8"
 
 type Mode = "pixiv" | "narou"
 
