dc155b0843ea65b2193c534d67820684ecbdf14e
Takahiro KOJIMA <12kojima.takahiro@gmail.com>
2025-02-01 19:07:33 +0900

    use peggy instead of pegjs

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,7 +1,20 @@
-module.exports = {
+export default {
   roots: ["<rootDir>/src"],
+  preset: "ts-jest/presets/js-with-ts-esm",
   transform: {
-    "^.+\\.tsx?$": "ts-jest",
+    "^.+\\.tsx?$": [
+      'ts-jest',
+      {
+        useESM: true,
+        tsconfig: 'tsconfig.json'
+      }
+    ]
+  },
+  transformIgnorePatterns: [
+    '<rootDir>/parser/(?!parser)'
+  ],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
   },
   testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
@@ -14,4 +27,4 @@ module.exports = {
   coverageReporters: [
     "lcov"
   ],
-}
+};
