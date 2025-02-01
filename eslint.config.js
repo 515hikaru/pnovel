67636bacd83ea9baa26beef9655ecde57699acfc
Takahiro KOJIMA <12kojima.takahiro@gmail.com>
2025-02-01 11:22:03 +0900

    rewrite config for ESLint

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,36 @@
-module.exports = {
-  env: {
-    browser: true,
-    commonjs: true,
-    es2021: true,
-    "jest/globals": true,
+// eslint.config.js
+import js from "@eslint/js"
+import ts from "@typescript-eslint/eslint-plugin"
+import tsParser from "@typescript-eslint/parser"
+import jest from "eslint-plugin-jest"
+import globals from "globals";
+
+export default [
+  js.configs.recommended,
+  {
+    files: ["src/**/*.ts", "src/__tests__/**/*.spec.ts"],
+    languageOptions: {
+      parser: tsParser,
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+    },
+    plugins: {
+      "@typescript-eslint": ts,
+      "jest": jest,
+    },
+    rules: {
+      "no-irregular-whitespace": "off",
+      "quotes": ["warn", "double"],
+      "comma-dangle": ["warn", "always-multiline"],
+      "semi": ["error", "never"],
+      "space-before-function-paren": ["error", "never"],
+      "@typescript-eslint/ban-ts-comment": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+    },
+    ignores: ["pnovel.js", "dist/*.js"],
   },
-  parser: "@typescript-eslint/parser",
-  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
-  globals: {
-    Atomics: "readonly",
-    SharedArrayBuffer: "readonly",
-  },
-  parserOptions: {
-    ecmaVersion: 12,
-  },
-  rules: {
-    "no-irregular-whitespace": 0,
-    quotes: ["warn", "double"],
-    "comma-dangle": ["warn", "always-multiline"],
-    semi: ["error", "never"],
-    "space-before-function-paren": ["error", "never"],
-    "@typescript-eslint/ban-ts-comment": "off",
-    "@typescript-eslint/no-explicit-any": 0,
-  },
-  plugins: ["@typescript-eslint", "jest"],
-  ignores: ["pnovel.js", "dist/*.js", "src/__tests__/*"]
-}
+]
